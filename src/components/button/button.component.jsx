efb2f8157988c2488f9a2639180609d59b6f71de
Jesus <jespinozaf07@gmail.com>
2022-05-23 11:53:03 -0600

    stripe payment add

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -4,6 +4,7 @@ import {
   BaseButton,
   GoogleSignInButton,
   InvertedButton,
+  ButtonSpinner,
 } from './button.styles';
 
 export const BUTTON_TYPE_CLASSES = {
@@ -19,8 +20,12 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-export const Button = ({ children, buttonType, ...otherProps }) => {
+export const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
 
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return (
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>
+  );
 };
